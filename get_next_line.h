bd4d90977447ebef60ae35fe9c26e4280ac72e37
Evgeniia Kashirskaia <ekashirs@c2r4p11.hive.fi>
2024-12-02 14:14:29 +0200

    initial commit

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekashirs <ekashirs@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 14:08:53 by ekashirs          #+#    #+#             */
+/*   Updated: 2024/12/02 14:12:25 by ekashirs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char *get_next_line(int fd);
+
+#endif
